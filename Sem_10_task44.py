fa72708d60567053816741f9c986d3a097270fa5
Victor <vic500@gmail.com>
2023-12-20 23:20:37 +0300

    new branch -  homework

--- Sem_10_task44.py
+++ Sem_10_task44.py
@@ -2,26 +2,4 @@
 которая состоит всего из 1 столбца. Ваша задача перевести его в one hot вид.
 Сможете ли вы это сделать без get_dummies?"""
 
-import pandas as pd
-import random
-
-lst = ['robot'] * 10
-lst += ['human'] * 10
-random.shuffle(lst)
-data = pd.DataFrame({'whoAmI': lst})
-data.head()
-print(data)
-
-unique_values = data['whoAmI'].unique()
-
-
-
-# Создание нового DataFrame, везде 0
-one_hot_data = pd.DataFrame(0, columns=unique_values, index=range(len(data)))
-one_hot_data2 = pd.get_dummies(data['whoAmI'])  # почему-то в зеркальном отображении
-
-# Проход по строкам и установка 1 для соответствующего столбца
-for ind, val in enumerate(data['whoAmI']):
-    one_hot_data.iloc[ind, one_hot_data.columns.get_loc(val)] = 1
-
-print(one_hot_data, one_hot_data2)
+# Решение в ветке homework
